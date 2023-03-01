8e8bbac4dc3cc2af8b0f5989fd0325fc8121c962
johnvonmuhlen <johnvonmuhlen@gmail.com>
2023-03-01 12:48:04 -0300

    primer commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CartWidget from './CartWidget';      
+
+const NavBar = () => {
+  return (
+    <div>
+      <CartWidget />
+    </div>
+  )
+};
+
+export default NavBar;
